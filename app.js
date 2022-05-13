97839d3ad2c7c99a5326c3ebdf1e5d091d8d5b1b
noelialf <noelia_luz_fernandez@hotmail.com>
2022-05-12 21:23:27 -0300

    Cambios para poder desplegar a Heroku

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-}); 
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Servidor funcionando');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
